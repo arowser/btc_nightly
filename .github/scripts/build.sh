79282ddbcd6bf9c883486e8d02a80ea6dbde48ba
danielsocials <daniel.socials@gmail.com>
2022-12-21 19:41:53 +0800

    cross build ccan configure

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -86,14 +86,14 @@ then
     wget -q https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
     tar xf gmp-6.2.1.tar.xz
     cd gmp-6.2.1 || exit 1
-    ./configure --prefix="$QEMU_LD_PREFIX" --host="$TARGET_HOST" CONFIGURATOR_CC="$TARGET_HOST-gcc -static"
+    ./configure --prefix="$QEMU_LD_PREFIX" --host="$TARGET_HOST"
     make
     sudo make install
     cd ..
     rm gmp-6.2.1.tar.xz
     rm -rf gmp-6.2.1
 
-    ./configure CC="$TARGET_HOST-gcc" --enable-static --disable-rust
+    ./configure CC="$TARGET_HOST-gcc" CONFIGURATOR_CC="$TARGET_HOST-gcc -static" --enable-static --disable-rust
 
     make -s -j32 CC="$TARGET_HOST-gcc"
 else
